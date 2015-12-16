4c21d0be3d1e23478a96c739dd4a7757c4f908d5
Mathieu <mathieu.hautebas@gmail.com>
2015-12-16 21:41:30 +0100

    blah

--- Makefile
+++ Makefile
@@ -1,6 +1,23 @@
 CC = g++
-
+	
 all:
-	@echo 'Building Odysseus-Utility...'
-	@$(CC) Odysseus.cpp -o Odysseus
-	@echo 'Succesfully built Odysseus'
+		@echo 'ERROR: no platform defined.'
+		@echo 'LINUX USERS: make linux'
+		@echo 'MAC OS X USERS: make macosx'
+	 	@echo 'WINDOWS USERS: make win'
+
+linux : 
+		@echo 'Building Odysseus Utility for Linux...'
+		@$(CC) Odysseus.cpp -o Odysseus
+		@echo 'Succesfully built Odysseus Utility for Linux'
+
+macos : 
+		@echo 'Building Odysseus Utility for OS X...'
+		@$(CC) Odysseus.cpp
+		@ mv a.out Odysseus
+		@echo 'Succesfully built Odysseus Utility for OS X'
+
+win :
+		@echo Building Odysseus Utility for Windows...
+		@$(CC) win_Odysseus.cpp -o Odysseus
+		@echo Succesfully built Odysseus Utility for Windows		
--- win_Odysseus.cpp
+++ win_Odysseus.cpp
@@ -32,15 +32,12 @@ using namespace std;
 void mainMenu();
 void odysseusLogo();
 void menuSelect();
-void linuxMenuSelect();
-void linuxSetup();
 void makeIPSW();
 void grabDemBlobs();
 void validateBlobs();
 void deviceRestore();
 void progressUI();
-void appleChoiceAction();
-void linuxChoiceAction();
+void winChoiceAction();
 
 int main() {
     system("clear");
@@ -52,12 +49,7 @@ void mainMenu()
 {
     odysseusLogo();
     printf("\n");
-#ifdef __APPLE__ && __MACH__ // I don't know if this will work on hackintoshes...
-    appleChoiceAction();
-#endif
-#ifdef __linux__
-    linuxChoiceAction();
-#endif
+    winChoiceAction();
 }
 
 void odysseusLogo ()
@@ -73,13 +65,14 @@ void odysseusLogo ()
     printf("  \\____/ \\__,_|\\__, |___/___/\\___|\\__,_|___/\n");
     printf("                __/ | toolchain by xerub   \n");
     printf("               |___/       utility by dayt0n\n");
+    printf("                 Windows version by matteyeux\n");
     printf("============================================\n");
 }
 
 void menuSelect()
 {
     printf("Do you want to: \n\n");
-    printf("1) Make iPSW and prepare kloader\n");
+    printf("1) Make IPSW and prepare kloader\n");
     printf("2) Grab on-device blobs\n");
     printf("3) Validate existing blobs\n");
     printf("4) Restore device to desired firmware\n");
@@ -87,30 +80,7 @@ void menuSelect()
     printf("Selection: ");
 }
 
-void linuxMenuSelect()
-{
-    printf("Do you want to: \n\n");
-    printf("1) Setup Odysseus environment\n");
-    printf("2) Make iPSW and prepare kloader\n");
-    printf("3) Grab on-device blobs\n");
-    printf("4) Validate existing blobs\n");
-    printf("5) Restore device to desired firmware\n");
-    printf("6) Quit\n\n");
-    printf("Selection: ");
-}
-
-void linuxSetup() // Code taken directly from my OdysseusLinuxSetup
-{
-    system("sudo apt-get update");
-    system("sudo apt-get -y install libimobiledevice-dev libssl-dev libusb-1.0.0 libzip2 libreadline6");
-    system("sudo apt-get -y install usbmuxd automake autoconf libtool pkg-config openssl build-essential libplist++-dev libzip-dev libcurl4-openssl-dev");
-    system("clear");
-    system("sudo ln -s /lib/x86_64-linux-gnu/libssl.so.1.0.0 /lib/x86_64-linux-gnu/libssl.so.1");
-    system("sudo ln -s /lib/x86_64-linux-gnu/libcrypto.so.1.0.0 /lib/x86_64-linux-gnu/libcrypto.so.1");
-    printf("Done. Odysseus should be fully configured.\n");
-}
-
-void appleChoiceAction()
+void winChoiceAction()
 {
     int userSelection;
     menuSelect();
@@ -146,51 +116,6 @@ void appleChoiceAction()
             printf("Invalid input. Try again.\n");
             break;
     }
-
-}
-
-void linuxChoiceAction()
-{
-    int userSelection;
-    linuxMenuSelect();
-    cin >> userSelection;
-    switch (userSelection) {
-        case 1:
-            system("clear");
-            odysseusLogo();
-            printf("\n");
-            linuxSetup();
-            break;
-        case 2:
-            system("clear");
-            odysseusLogo();
-            printf("\n");
-            makeIPSW();
-            break;
-        case 3:
-            system("clear");
-            odysseusLogo();
-            printf("\n");
-            grabDemBlobs();
-            break;
-        case 4:
-            system("clear");
-            odysseusLogo();
-            printf("\n");
-            validateBlobs();
-            break;
-        case 5:
-            system("clear");
-            odysseusLogo();
-            printf("\n");
-            deviceRestore();
-            break;
-        case 6:
-            break;
-        default:
-            printf("Invalid input. Try again.\n");
-            break;
-    }
 }
 
 void makeIPSW()
@@ -216,7 +141,7 @@ void makeIPSW()
     system("clear");
     if (isOTA == 'n') {
         printf("Grabbing baseband.tar...\n");
-        system(("./sshtool -s baseband.tar -p 22 "+deviceIP).c_str());
+        system(("sshtool -s baseband.tar -p 22 "+deviceIP).c_str());
         system("clear");
     printf("Creating iPSW...\n"); // Below: If-ception
     if (moarRAM != 'y')
@@ -224,18 +149,18 @@ void makeIPSW()
         if (bareJB != 'y')
         {
             if (ifstream("baseband.tar")) {
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw baseband.tar").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw baseband.tar").c_str());
             }
             else
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw").c_str());
         }
         else
         {
             if (ifstream("baseband.tar")) {
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw baseband.tar ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw baseband.tar ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
             }
             else
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
         }
     }
     else
@@ -243,18 +168,18 @@ void makeIPSW()
         if (bareJB != 'y')
         {
             if (ifstream("baseband.tar")) {
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw -memory baseband.tar").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw -memory baseband.tar").c_str());
             }
             else
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw -memory").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw -memory").c_str());
         }
         else
         {
             if (ifstream("baseband.tar")) {
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw -memory baseband.tar ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw -memory baseband.tar ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
             }
             else
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw -memory ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw -memory ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
         }
     }
     }
@@ -265,22 +190,22 @@ void makeIPSW()
         {
             if (bareJB != 'y')
             {
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw -bbupdate").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw -bbupdate").c_str());
             }
             else
             {
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw -bbupdate ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw -bbupdate ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
             }
         }
         else
         {
             if (bareJB != 'y')
             {
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw -memory -bbupdate").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw -memory -bbupdate").c_str());
             }
             else
             {
-                system(("./ipsw "+downloadedIPSW+" custom.ipsw -memory -bbupdate ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
+                system(("ipsw "+downloadedIPSW+" custom.ipsw -memory -bbupdate ../jb/p0sixspwn.tar ../jb/ssh_small.tar").c_str());
             }
         }
 
@@ -288,7 +213,7 @@ void makeIPSW()
     if (ifstream("custom.ipsw")) {
         system("clear");
         printf("Extracting PWNed iBSS image...\n");
-        system("./xpwntool `unzip -j custom.ipsw 'Firmware/dfu/iBSS*' | awk '/inflating/{print $2}'` pwnediBSS");
+        system("xpwntool `unzip -j custom.ipsw 'Firmware/dfu/iBSS*' | awk '/inflating/{print $2}'` pwnediBSS");
         system("clear");
         printf("Image extracted. \nYou now have a custom iPSW and PWNed iBSS to kickstart the restore.\n");
     }
@@ -308,66 +233,42 @@ void grabDemBlobs() // This part MAYBE works...
         printf("\nExtracting PWNed iBEC...\n");
         system("mv `unzip -j custom.ipsw 'Firmware/dfu/iBEC*' | awk '/inflating/{print $2}'` pwnediBEC");
         printf("Kloading PWNed iBSS...(might want to CTRL+C if it hangs at the end)\n");
-        system(("./sshtool -k ../kloader -b pwnediBSS -p 22 "+deviceIP).c_str());
+        system(("sshtool -k ../kloader -b pwnediBSS -p 22 "+deviceIP).c_str());
         sleep(10);
         system("clear");
         printf("Unplug and replug the USB cable connected the iDevice. Then, press [Enter].\n");
         cin.ignore();
         system("clear");
         printf("Sending PWNed iBEC...\n");
-#ifdef __APPLE__ && __MACH__
-        system("./irecovery -f pwnediBEC");
-        sleep(6);
-        printf("Sending payload...\n");
-        system("./irecovery -f ../payload"); // For some reason this is currently failing in OS X 10.11 beta 2. Will update and check later.
-        sleep(3);
-        system("./irecovery -c \"go blobs\"");
-        sleep(3);
-        printf("Grabbing blobs...\n");
-        system("./irecovery -g precious.dump");
-        sleep(3);
-        if (fstream("precious.dump"))
-        {
-            printf("Got blobs. Booting back to userland.\n");
-            system("./irecovery -c \"reboot\"");
-            sleep(1);
-        }
-        else
-        {
-            system("./irecovery -c \"reboot\"");
-            printf("\nFailed to obtain blobs.\n");
-            return;
-        }
-#endif
-#ifdef __linux__
-        system("sudo ./irecovery -f pwnediBEC"); // Should probably say this now: but in my experience, Linux requires root to access USB. This means that you will have to have an option for OS X and Linux everytime you use a tool like irecovery or idevicerestore that interacts with USB.
+
+        system("irecovery -f pwnediBEC"); // Should probably say this now: but in my experience, Linux requires root to access USB. This means that you will have to have an option for OS X and Linux everytime you use a tool like irecovery or idevicerestore that interacts with USB.
         sleep(5);
         printf("Sending payload...\n");
-        system("sudo ./irecovery -f ../payload");
+        system("irecovery -f ../payload");
         sleep(3);
-        system("sudo ./irecovery -c \"go blobs\"");
+        system("irecovery -c \"go blobs\"");
         sleep(3);
         printf("Grabbing blobs...\n");
-        system("sudo ./irecovery -g precious.dump");
+        system("irecovery -g precious.dump");
         sleep(3);
         if (fstream("precious.dump")) {
             printf("Got blobs. Booting back to userland.\n");
-            system("sudo ./irecovery -c \"reboot\"");
+            system("irecovery -c \"reboot\"");
             sleep(1);
         }
         else
         {
-            system("sudo ./irecovery -c \"reboot\"");
+            system("irecovery -c \"reboot\"");
             printf("\nFailed to obtain blobs.\n");
             return;
         }
-#endif
+
         system("clear");
         printf("Drag the downloaded iPSW of the iOS version that your device is currently on: ");
         cin.ignore(100,'\n');
         getline(cin, downloadedBlobIPSW);
         printf("\nUnpacking dumped blobs...\n");
-        system(("./ticket precious.dump precious.plist "+downloadedBlobIPSW+" -z").c_str());
+        system(("ticket precious.dump precious.plist "+downloadedBlobIPSW+" -z").c_str());
         printf("Done grabbing blobs.\n");
     }
     else
@@ -393,30 +294,20 @@ void validateBlobs()
     cin >> isBlock;
     if (isBlock != 'n')
     {
-        printf("\nWe can't validate the TinyUmbrella block, but we can convert it to XML if you want [y or n]: ");
-        cin >> validateTU;
-        if (validateTU == 'y')
-        {
-            system(("zcat "+meBlob+" > TU_XML.shsh").c_str());
-            system("plutil -convert xml1 TU_XML.shsh");
-            system("clear");
-            printf("\nBlob converted to XML format under the name TU_XML.shsh in the Odysseus working directory.\n");
-        }
-        else
-            printf("\nTry extracting your preferred blob from the \"Block\", then try again.\n");
+        printf("\nUse SHSHaker to convert blobs (check the README)");
     }
-    printf("\nIs the blob in XML format? [y or n]: ");
+    printf("Is the blob in XML format? [y or n]: ");
     cin >> isXML;
-    if (isXML != 'y')
+    if (isXML != 'y') /*Did not tested this part, idk if there is plutil in Cygwin or Mingw*/
     {
         system(("zcat "+meBlob+" > XML.shsh").c_str());
         system("plutil -convert xml1 XML.shsh");
-        system(("./validate XML.shsh "+downloadedBlobIPSW+" -z").c_str());
+        system(("validate XML.shsh "+downloadedBlobIPSW+" -z").c_str());
         printf("\nConverted Blob saved as XML.shsh in Odysseus working directory.\n");
     }
     else
     {
-        system(("./validate "+meBlob+" "+downloadedBlobIPSW+" -z").c_str());
+        system(("validate "+meBlob+" "+downloadedBlobIPSW+" -z").c_str());
     }
     printf("\nBlobs validated.\n");
 }
@@ -431,7 +322,7 @@ void deviceRestore() // This *should* restore the device to the desired firmware
         printf("Enter the desired device's local IP address: ");
         cin.ignore(100,'\n');
         getline(cin, deviceIP);
-        system(("./sshtool -k ../kloader -b pwnediBSS -p 22 "+deviceIP).c_str());
+        system(("sshtool -k ../kloader -b pwnediBSS -p 22 "+deviceIP).c_str());
         sleep(10);
         system("clear");
         odysseusLogo();
@@ -446,13 +337,8 @@ void deviceRestore() // This *should* restore the device to the desired firmware
         sleep(3);
         printf("Initiating Restore...\n"); // I don't check for blobs in this function because idevicerestore does that for me.
         sleep(2);
-#ifdef __APPLE__ && __MACH__
-        system("killall iTunesHelper");
-        system("./idevicerestore -d -w custom.ipsw");
-#endif
-#ifdef __linux__
-        system("sudo ./idevicerestore -d -w custom.ipsw");
-#endif
+        system("idevicerestore -d -w custom.ipsw");
+
         printf("\nRestore Completed. This should have worked if you did everything correctly...\n");
         }
         else
